cfcf1e563a0621590791933e728942cb1a555298
Atilla-Koz <atillakoz@hotmail.com>
2024-03-07 14:17:35 +0300

    component dosyları ve rootlar oluşturuldu

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router } from "react-router-dom";
+ReactDOM.render(
+  <Router>
+  <App />
+  </Router>
+  , document.getElementById("root"));
+  

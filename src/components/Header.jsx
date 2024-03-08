c470bf78cc1736aa64d87c0298e28cc7c3b3d16a
Atilla-Koz <atillakoz@hotmail.com>
2024-03-08 12:14:47 +0300

    Header componet  ve css dosyası oluşturuldu

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from 'reactstrap';
+import logo from './logo.svg';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img className="logo" src={logo} alt="logo" />
+      <section className="headerLink">
+        <NavLink href="/">Ana Sayfa</NavLink>
+        <NavLink href="">Seçenekler</NavLink>
+        <NavLink href="/order">Sipariş Oluştur</NavLink>
+      </section>
+    </div>
+  );
+}
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -9,82 +9,88 @@ import {
   Button,
 } from 'reactstrap';
 import './order.css';
+import Header from './Header';
 export default function Order() {
   return (
-    <div className="orderContainer">
-      <Form>
-        <FormGroup row>
-          <Label for="exampleSelect" sm={2}>
-            Select
-          </Label>
-          <Col sm={10}>
-            <Input id="exampleSelect" name="select" type="select">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </Input>
-          </Col>
-        </FormGroup>
+    <>
+      <header>
+        <Header />
+      </header>
+      <div className="orderContainer">
+        <Form>
+          <FormGroup row>
+            <Label for="exampleSelect" sm={2}>
+              Select
+            </Label>
+            <Col sm={10}>
+              <Input id="exampleSelect" name="select" type="select">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Input>
+            </Col>
+          </FormGroup>
 
-        <FormGroup row>
-          <Label for="exampleText" sm={2}>
-            Text Area
-          </Label>
-          <Col sm={10}>
-            <Input id="exampleText" name="text" type="textarea" />
-          </Col>
-        </FormGroup>
+          <FormGroup row>
+            <Label for="exampleText" sm={2}>
+              Text Area
+            </Label>
+            <Col sm={10}>
+              <Input id="exampleText" name="text" type="textarea" />
+            </Col>
+          </FormGroup>
 
-        <FormGroup row tag="fieldset">
-          <legend className="col-form-label col-sm-2">Radio Buttons</legend>
-          <Col sm={10}>
-            <FormGroup check>
-              <Input name="radio2" type="radio" />{' '}
-              <Label check>
-                Option one is this and that—be sure to include why it‘s great
-              </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Input name="radio2" type="radio" />{' '}
-              <Label check>
-                Option two can be something else and selecting it will deselect
-                option one
-              </Label>
-            </FormGroup>
-            <FormGroup check disabled>
-              <Input disabled name="radio2" type="radio" />{' '}
-              <Label check>Option three is disabled</Label>
-            </FormGroup>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="checkbox2" sm={2}>
-            Checkbox
-          </Label>
-          <Col
-            sm={{
-              size: 10,
-            }}
-          >
-            <FormGroup check>
-              <Input id="checkbox2" type="checkbox" />{' '}
-              <Label check>Check me out</Label>
-            </FormGroup>
-          </Col>
-        </FormGroup>
-        <FormGroup check row>
-          <Col
-            sm={{
-              offset: 2,
-              size: 10,
-            }}
-          >
-            <Button>Submit</Button>
-          </Col>
-        </FormGroup>
-      </Form>
-    </div>
+          <FormGroup row tag="fieldset">
+            <legend className="col-form-label col-sm-2">Radio Buttons</legend>
+            <Col sm={10}>
+              <FormGroup check>
+                <Input name="radio2" type="radio" />{' '}
+                <Label check>
+                  Option one is this and that—be sure to include why it‘s great
+                </Label>
+              </FormGroup>
+              <FormGroup check>
+                <Input name="radio2" type="radio" />{' '}
+                <Label check>
+                  Option two can be something else and selecting it will
+                  deselect option one
+                </Label>
+              </FormGroup>
+              <FormGroup check disabled>
+                <Input disabled name="radio2" type="radio" />{' '}
+                <Label check>Option three is disabled</Label>
+              </FormGroup>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="checkbox2" sm={2}>
+              Checkbox
+            </Label>
+            <Col
+              sm={{
+                size: 10,
+              }}
+            >
+              <FormGroup check>
+                <Input id="checkbox2" type="checkbox" />{' '}
+                <Label check>Check me out</Label>
+              </FormGroup>
+            </Col>
+          </FormGroup>
+          <FormGroup check row>
+            <Col
+              sm={{
+                offset: 2,
+                size: 10,
+              }}
+            >
+              <Button>Submit</Button>
+            </Col>
+          </FormGroup>
+        </Form>
+      </div>
+    </>
   );
 }

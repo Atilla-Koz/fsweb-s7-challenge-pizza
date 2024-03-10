85f95f0ff4ac9c45a9ae3033856860f07df73962
Atilla-Koz <atillakoz@hotmail.com>
2024-03-10 23:48:05 +0300

    order top css update

--- src/components/OrderTop.jsx
+++ src/components/OrderTop.jsx
@@ -2,7 +2,10 @@ export default function OrderTop() {
   return (
     <>
       <h2>Position Absolute Acı Pizza</h2>
-      <h3>85.50₺</h3>
+      <nav className="otp-nav">
+        <h3>85.50₺</h3> <p>4.9</p>
+        <p>(200)</p>
+      </nav>
       <p className="OTP">
         Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı
         pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer

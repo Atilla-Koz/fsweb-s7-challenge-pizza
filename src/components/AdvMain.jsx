da42a0378e4c100e11f87da918f8668a83ae1fd9
Atilla-Koz <atillakoz@hotmail.com>
2024-03-11 23:17:32 +0300

    ADVmain component update

--- src/components/AdvMain.jsx
+++ src/components/AdvMain.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import logo from './logo.svg';
+import icon1 from '../mile2-aseets/icons/1.svg';
+import icon2 from '../mile2-aseets/icons/2.svg';
+import icon3 from '../mile2-aseets/icons/3.svg';
+import icon4 from '../mile2-aseets/icons/4.svg';
+import icon5 from '../mile2-aseets/icons/5.svg';
+import icon6 from '../mile2-aseets/icons/6.svg';
 
 const customButtonStyle = {
   marginTop: '1rem',
@@ -16,15 +22,46 @@ const customButtonStyle = {
 
 export default function AdvMain() {
   return (
-    <div className="main">
-      <section>
-        <img className="logo" src={logo} alt="" />
-      </section>
-      <p className="mainP">KOD ACIKTIRIR </p>
-      <p className="mainP">PIZZA, DOYURUR</p>
-      <Link to="/order">
-        <Button style={customButtonStyle}>Acıktım</Button>
-      </Link>
+    <div className="AdvContainer">
+      <header>
+        <div className="AdvMain">
+          <section>
+            <img className="logo" src={logo} alt="" />
+          </section>
+          <p className="mainP">KOD ACIKTIRIR </p>
+          <p className="mainP">PIZZA, DOYURUR</p>
+          <Link to="/order">
+            <Button style={customButtonStyle}>Acıktım</Button>
+          </Link>
+        </div>
+        <section className="headBot">
+          <img src={icon1} alt="Your Icon" />
+          <img src={icon2} alt="Your Icon" />
+          <img src={icon3} alt="Your Icon" />
+          <img src={icon4} alt="Your Icon" />
+          <img src={icon5} alt="Your Icon" />
+          <img src={icon6} alt="Your Icon" />
+        </section>
+        <section className="cards">
+          <div className="leftCard">
+            <legend>deneme</legend>
+            <p>denem</p>
+            <button>deneme</button>
+          </div>
+          <div className="rightCard">
+            <div className="rightTop">
+              <legend>deneme</legend>
+              <p>denem</p>
+              <button>deneme</button>
+            </div>{' '}
+            <div className="rightBot">
+              <legend>deneme</legend>
+              <p>deneme</p>
+              <button>deneme</button>
+            </div>
+          </div>
+        </section>
+      </header>
     </div>
   );
 }

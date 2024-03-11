4c94963f459cb600585cf442024527ed460cf1a7
Atilla-Koz <atillakoz@hotmail.com>
2024-03-11 15:24:36 +0300

    main.jsx root change  form

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter as Router } from 'react-router-dom';
-ReactDOM.render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
